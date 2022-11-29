d9b2d5bc129cbc9ad4fe9fe6eeb704720176c71e
Mehul-00 <118922007+Mehul-00@users.noreply.github.com>
2022-11-29 22:23:22 +0530

    Add files via upload

---
+++ lecture10-escape_sequence-CipherSchools.py
@@ -0,0 +1,7 @@
+print("hello \"world\" world")
+print('I\'m Mehul')
+print("line A\nline B\nline C")
+print("Name\tMehul")
+print("this is backslash\\")
+print("this is double backslash\\\\")
+print("hell\blo")
